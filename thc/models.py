4e385438e0201e3bd7c405158891e3a414977309
mrtovsky <mrtovsky@protonmail.com>
2020-10-06 00:21:36 +0200

    :sparkles: Adapt the DistilBERT to the task
    Create a model inheriting pre-trained weights from the Multilingual
    Cased DistilBERT that in addition contains fully-connected layer with
    dropout on top to match the classification task.

---
+++ thc/models.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+import torch
+import torch.nn as nn
+from transformers import DistilBertModel
+
+
+class DistilBertClassifier(nn.Module):
+    def __init__(self, output_size: int) -> None:
+        super().__init__()
+        self.distilbert = DistilBertModel.from_pretrained(
+            "distilbert-base-multilingual-cased"
+        )
+        self.dropout = nn.Dropout(p=0.1)
+        self.fc = nn.Linear(self.distilbert.config.hidden_size, output_size)
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        _, pooled = self.distilbert(
+            input_ids=input_ids, attention_mask=attention_mask
+        )
+        output = self.fc(self.dropout(pooled))
+
+        return output
